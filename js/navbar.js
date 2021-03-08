d5b40985f65a8fd6bb7f6d38b4434a700bff28e1
gabrielajasnosz <gabriela.jasnosz@gmail.com>
2021-03-08 09:08:25 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+//let nav='<nav><a href="index.html">Home</a>' + '<a href="contact.html">Contact</a></nav>';
+let nav='<nav><ul> <li><a href="contact.html">Contact</a></li> <li><a href="index.html">Home</a></li> </ul></nav>'
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
+
