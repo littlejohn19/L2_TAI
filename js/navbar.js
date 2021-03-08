4d21e720942961db6ae40a17063baacedd131c1c
budzikbudzik <kamil.budzik98@gmail.com>
2021-03-08 14:57:23 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav><ul> <li><a href="contact.html">Contact</a></li> <li><a href="index.html">Home</a></li> </ul></nav>'
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
