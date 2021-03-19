73a342808b934b83c62a81e3236afc8c14299cd4
Domel <domel1162@gmail..com>
2021-03-19 19:40:47 +0100

    c

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+let nav = '<li class="menutext"><a href="../html/index.html">Strona główna</a> </li>' +
+    '<li class="menutext"><a href="../html/contact.html">Kontakt</a> </li></nav>'+
+    '<li class="menutext"><a href="../html/quiz.html">Quiz</a> </li></nav>';
+
+let elem = document.querySelector(".navbar");
+
+elem.innerHTML = nav;
--- navbar.js
+++
@@ -1,6 +0,0 @@
-let nav = '<li class="menutext"><a href="index.html">Strona główna</a> </li>' +
-    '<li class="menutext"><a href="contact.html">Kontakt</a> </li></nav>';
-
-let elem = document.querySelector(".navbar");
-
-elem.innerHTML = nav;
